0e6f6852540f502db837cce09d8387feacd28b4a
stm_int <114091453+stm7128@users.noreply.github.com>
2024-04-05 02:00:52 +0900

    Update fix.user.js

--- fix.user.js
+++ fix.user.js
@@ -10,15 +10,9 @@
 
 (function() {
     'use strict';
-
-    // すべての画像要素を取得する
     const images = document.querySelectorAll('img');
-
-    // 各画像要素について処理を行う
     images.forEach(img => {
-        // data-sco-src属性が存在する場合
         if (img.hasAttribute('data-sco-src')) {
-            // src属性をdata-sco-src属性の値に置き換える
             img.setAttribute('src', img.getAttribute('data-sco-src'));
         }
     });
