caf918c7c6b39af41fe2dcafb4d3755a26f4a2a3
stm7128 <114091453+stm7128@users.noreply.github.com>
2024-04-05 17:15:04 +0900

    Create enablecopy.user.js

---
+++ enablecopyinutanet/enablecopy.user.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         enable copy lyric in uta-net.com
+// @namespace    https://github.com/stm7128/userscripts/
+// @version      1.0
+// @description  uta-net.comにおいて歌詞をコピーできるようにする
+// @author       stm7128
+// @match        *://uta-net.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var kashiAreaDiv = document.getElementById('kashi_area');
+    if (kashiAreaDiv) {
+        kashiAreaDiv.removeAttribute('id');
+    }
+})();
